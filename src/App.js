6e592611309122e1aaa1a1265a5f6277a5996604
Ivan Poberezhniuk <i.poberezhniuk.work@gmail.com>
2021-01-20 14:41:15 +0200

    moved App styled component to separated file in /styled/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import './App.scss';
 
-import styled from 'styled-components';
-
 import Header from './components/particials/Header';
 import Footer from './components/particials/Footer';
 import GalleryPage from './pages/GalleryPage';
-
-const StyledDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`;
+import {AppDiv} from './styled/App';
 
 const App = () => (
-  <StyledDiv className="App">
+  <AppDiv className="App">
     <Header/>
     <main>
       <GalleryPage/>
     </main>
     <Footer/>
-  </StyledDiv>
+  </AppDiv>
 );
 
 export default App;
