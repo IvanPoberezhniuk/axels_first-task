dc28b6eef7f0fddd3bd22d05a1ce8382e1ee55b2
Ivan Poberezhniuk <i.poberezhniuk.work@gmail.com>
2021-01-27 20:20:28 +0200

    update location type

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,35 @@ import { Footer, Header, ImageModal } from './components';
 import { routes, RouteWithSubRoutes } from './routes/routes';
 import { AppDiv } from './styled/App';
 import './App.scss';
+import { Hash, LocationKey, Pathname, Search } from 'history';
 
-interface ILocationState {
-  background: string
-}
+type ILocationState = {
+  pathname: Pathname;
+  search: Search;
+  state?: any;
+  hash: Hash;
+  key?: LocationKey;
+};
+
+type bgT = {
+  background: string;
+};
 
 const App = () => {
-  const location = useLocation<ILocationState>();
+  const location: ILocationState | bgT = useLocation();
   const backgroundPage = location.state && location.state.background;
 
   return (
     <AppDiv>
       <Header />
       <main>
-        <Switch location={(backgroundPage || location)}>
+        <Switch location={backgroundPage || location}>
           {routes.map((route, i) => (
             <RouteWithSubRoutes key={i} {...route} />
           ))}
         </Switch>
         {backgroundPage && (
-          <Route path='/gallery/:id' children={<ImageModal />} />
+          <Route path="/gallery/:id" children={<ImageModal />} />
         )}
       </main>
       <Footer />
